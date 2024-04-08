6dbf52e6712d04298c75e171333ba57f9843a833
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-04-08 13:11:29 +0200

    Fix key warnings

--- express/app.js
+++ express/app.js
@@ -1,52 +1,67 @@
 import React, { Suspense, use } from "react";
 
 export default function App() {
+  console.log("app");
+
   return React.createElement("html", null, [
-    React.createElement("head", null, [
-      React.createElement("meta", { charSet: "utf-8" }),
+    React.createElement("head", { key: "1" }, [
+      React.createElement("meta", { key: "1", charSet: "utf-8" }),
       React.createElement("meta", {
+        key: "2",
         name: "viewport",
         content: "width=device-width, initial-scale=1",
       }),
-      React.createElement("title", null, "My app"),
+      React.createElement("title", { key: "3" }, "My app"),
     ]),
-    React.createElement("body", null, [
-      React.createElement("div", { id: "root" }, "Some content"),
+    React.createElement("body", { key: "2" }, [
+      React.createElement(
+        "button",
+        { key: "1", onClick: () => alert("hi") },
+        "Click me!"
+      ),
       React.createElement(
         Suspense,
-        { fallback: React.createElement("div", null, "Loading...") },
+        { key: "2", fallback: React.createElement("div", null, "Loading...") },
         React.createElement(Child)
       ),
-      React.createElement("div", null, "Later content"),
+      React.createElement("div", { key: "3" }, "Later content"),
     ]),
   ]);
 }
 
-function suspend(ms) {
-  if (typeof window !== "undefined") {
-    return Promise.resolve();
-  }
-
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 function Child() {
-  use(suspend(2000));
+  use(waitFor(2000));
+
+  console.log("child");
 
   return React.createElement("div", null, [
-    React.createElement("p", null, "Child"),
-    React.createElement(Suspense, { fallback: "Loading grandchild..." }, [
-      React.createElement(Grandchild),
-    ]),
+    React.createElement("p", { key: "1" }, "Child"),
+    React.createElement(
+      Suspense,
+      { key: "2", fallback: "Loading grandchild..." },
+      React.createElement(Grandchild)
+    ),
   ]);
 }
 
 function Grandchild() {
-  use(suspend(2000));
+  use(waitFor(2000));
+
+  console.log("grandchild");
 
   return React.createElement(
     "button",
     { onClick: () => alert("hello") },
     "Click me!"
   );
+}
+
+const resolvedPromise = Promise.resolve();
+
+function waitFor(ms) {
+  if (typeof window !== "undefined") {
+    return resolvedPromise;
+  }
+
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
